02469db0829c4b841976b4f24c9df7d13f5bb410
harika <harikasuraparaju@gmail.com>
2025-08-22 15:32:18 +0530

    project folder created

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+
+function App() {
+
+
+  return (
+   <>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/"   element={<Home/>} />
+    <Route path="/login"   element={<Login/>} />
+   </Routes>
+   </BrowserRouter>
+   </>
+    
+    
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const Home = () => {
+  return (
+    <>
+    <h1 class="text-3xl font-bold ">
+    Hello world!
+     </h1>
+    </>
+  )
+}
+
+export default Home
